010a1dca9e3a75df167d0c9c573e635d68923e89
arthurmousinho <arthursilvamo296@gmail.com>
2026-09-17 15:47:18 -0300

    feat: add user service

---
+++ backend/src/main/java/com/slabi/user/UserService.java
@@ -0,0 +1,24 @@
+package com.slabi.user.service;
+
+import com.slabi.user.entity.User;
+import com.slabi.user.repository.UserRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
